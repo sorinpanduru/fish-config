329e9884bc6b39f510a272ae8033d750a4fb1316
vially <vially.ichb@gmail.com>
2013-05-21 17:46:01 +0300

    Fix git awk command for mac os

--- config.fish
+++ config.fish
@@ -12,7 +12,7 @@ function parse_git_dirty
 end
 function parse_git_branch
          # git branch outputs lines, the current branch is prefixed with a *
-         set -l branch (git branch --color ^&- | awk '/*/ {print $2}') 
+         set -l branch (git branch --color ^&- | awk '/\*/ {print $2}') 
          echo $branch (parse_git_dirty)     
 end
 
