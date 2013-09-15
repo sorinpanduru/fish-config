a52dda631f02bbf7c7104293f9a528b74144ba11
vially <vially.ichb@gmail.com>
2013-09-15 08:23:40 +0300

    Move mac-os configs to local file

--- config.fish
+++ config.fish
@@ -11,12 +11,8 @@ set __fish_git_prompt_color_dirtystate red
 # Disable greeting
 set fish_greeting
 
-# RVM
-rvm > /dev/null
-
 # Aliases
 alias dj='python manage.py'
 
 # Variables
 set -x EDITOR vim
-set -x PATH ~/.rvm/bin /usr/local/share/npm/bin $PATH 
